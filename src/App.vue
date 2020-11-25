469b76a0e488ad0db07d36cedd27d1b7ab992ed6
Luis Lovera <LLOVERA@hubgroup.com>
2020-11-25 14:45:21 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <Main />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Main from "./components/Main";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld,
+    Main,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+html,
+body {
+  font-family: Open Sans, Arial, sans-serif;
+  font-size: 14px;
+  color: #666;
+  background-color: #fff;
+  line-height: 1.7em;
+  font-weight: 500;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.default-text-medium {
+  font-family: "DM Sans", Helvetica, Arial, Lucida, sans-serif;
+  font-size: 18px;
+  line-height: 1.8em;
+  color: rgba(22, 28, 45, 0.7) !important;
+}
+
+.percent-box {
+  .percent {
+    margin: auto;
+    height: 72px;
+    p {
+      font-family: "DM Sans", Helvetica, Arial, Lucida, sans-serif;
+      font-weight: 700;
+      font-size: 50px;
+      color: #161c2d !important;
+      letter-spacing: -2px;
+      line-height: 42px;
+      text-align: center;
+    }
+  }
+  .percent-description {
+    margin: auto;
+    margin-bottom: 30px;
+    max-width: 275px;
+  }
 }
 </style>
